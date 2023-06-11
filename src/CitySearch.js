ba27d839526ea4405af82387553b78a9c322deac
Macir Brustolin <mj.brustolin@hotmail.com>
2023-06-11 17:57:58 -0300

    CitySearch created

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 
 const CitySearch = ( { getAirQuality } ) => {
-
+    return (
+        <form>
+            <input></input>
+            <button type='submit'>Search</button>
+        </form>
+    )
 }
